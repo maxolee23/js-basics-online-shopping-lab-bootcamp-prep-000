dcd238bc895b396bee4481f4602eac2f59f645d1
Max Lee <maxolee23@gmail.com>
2019-06-10 15:51:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,31 +1,64 @@
-  return t;
-};
-
-var cart = []
-
-function getCart() {
-  return cart;
-
-}
-
 function addToCart(item) {
-  var price = Math.floor(Math.random() * 100)
-  cart.push({[item]: price})
-  console.log(item + " has been added to your cart.")
-  return cart
+ // write your code here
+  var price = Math.floor(Math.random() * (100 - 1)) + 1
+  var cartItem = {itemName: item, itemPrice: price}
+
+  cart.push(cartItem)
+
+  return `${item} has been added to your cart.`
+
 }
 
-var yourCart = []
 function viewCart() {
+  // write your code here
+  var array = [];
 
-  if (cart.length === 0) {
-    console.log("Your shopping cart is empty.")
+  for ( let i = 0; i < cart.length; i++) {
+      array.push(`${cart[i].itemName} at $${cart[i].itemPrice}`)
   }
-  else {
-for (var i = 0; i < cart.length; i++) {
-    for (var key in cart) {
-      yourCart.push(`${key} at $${cart[key]}`)
-    console.log("In your cart, you have " + yourCart.join() + ".")
+  if (cart.length === 0) {
+    return `Your shopping cart is empty.`;}
 
+  if (cart.length === 1){
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
+  }
+
+  if (cart.length ===2){
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
+  }else{
+    let lastItem = array.pop();
+    return `In your cart, you have ${array.join(', ')}, and ${lastItem}.`
+  }
 }
-} }
+
+
+function total() {
+  // write your code here
+ let totalCost = 0;
+  for (let i = 0; i < cart.length; i++){
+    totalCost += cart[i].itemPrice;
+  }
+  return totalCost;
+}
+
+function removeFromCart(item) {
+  // write your code here
+  for (let i = 0; i < cart.length; i++){
+    if (cart[i].itemName === item){
+      cart.splice(i,1);
+      return cart;
+    }
+  }
+  return "That item is not in your cart.";
+}
+
+function placeOrder(cardNumber) {
+  // write your code here
+  if (cardNumber){
+    let totalCost = total();
+    cart =[];
+    return`Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`;
+  } else {
+    return "Sorry, we don't have a credit card on file for you.";
+  }
+}
